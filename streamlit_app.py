e48e93704093fb0486793b97c3381b3706c873cd
yellow <huangwanyi@me.com>
2024-10-24 08:55:35 +0000

    file upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,15 @@ st.set_page_config(
     page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
 )
 
+uploaded_files = st.file_uploader(
+    "Choose a CSV file", accept_multiple_files=True
+)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
+
+
 # -----------------------------------------------------------------------------
 # Declare some useful functions.
 
